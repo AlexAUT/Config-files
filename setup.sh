a75d5dca283ffeb72e08efa7c0a792f6cc4a660e
Alexander Weinrauch <alexander.weinrauch@gmail.com>
2017-10-07 22:14:31 +0200

    Added background and transparent bar

--- setup.sh
+++ setup.sh
@@ -17,9 +17,13 @@ rm -f $HOME/.config/sway/config
 mkdir -p $HOME/.config/sway
 ln -s $PWD/sway.config $HOME/.config/sway/config
 
+echo "Desktop background"
+rm -r $HOME/.config/sway/background.jpg
+ln -s $PWD/background.jpg $HOME/.config/sway/background.jpg
+
 echo "Custom network bandwidth plugin"
 rm -f $HOME/.config/sway/bandwidth
-ln -s $PWD/bandwidth $HOME/.config/sway/bandwidth
+ln -s $PWD/bandwidth $HOME/.config/sway/bandwidt
 
 # link i3blocks config file
 echo "i3Blocks config"
