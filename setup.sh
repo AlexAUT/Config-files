06cbadb16ea417b360b2b752b0ec30fc6dccaed5
Alexander Weinrauch <alexander.weinrauch@gmail.com>
2017-10-07 19:50:44 +0000

    Added bandwidth script and improved icons

--- setup.sh
+++ setup.sh
@@ -11,14 +11,23 @@ rm -rf font-awesome-4.7.0/
 rm -f font-awesome-4.7.0.zip
 echo "Done..."
 echo "Seting up config files"
-# sway config
+
+echo "Sway config"
 rm -f $HOME/.config/sway/config
 mkdir -p $HOME/.config/sway
 ln -s $PWD/sway.config $HOME/.config/sway/config
+
+echo "Custom network bandwidth plugin"
+rm -f $HOME/.config/sway/bandwidth
+ln -s $PWD/bandwidth $HOME/.config/sway/bandwidth
+
 # link i3blocks config file
+echo "i3Blocks config"
 rm -f $HOME/.i3blocks.conf
 ln -s $PWD/i3blocks.conf $HOME/.i3blocks.conf
+
 # Create path to config file and remove if present
+echo "termite config"
 mkdir -p $HOME/.config/termite/
 rm -f $HOME/.config/termite/config
 ln -s $PWD/termite.conf $HOME/.config/termite/config
