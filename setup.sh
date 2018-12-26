e5cef122217eb4f147888917ab6848aed2512c04
Alexander Weinrauch <alexander.weinrauch@gmail.com>
2018-12-26 13:32:46 +0100

    Now supports i3 and sway!

--- setup.sh
+++ setup.sh
@@ -1,12 +1,49 @@
 #!/bin/sh
 PROJECT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
+# Set the config path to default if non is set
+if [ -z $XDG_CONFIG_HOME ]
+then
+  XDG_CONFIG_HOME=$HOME/.config/
+fi
+
+I3_CONFIG_FOLDER=$HOME/.config/i3
+I3_CONFIG_FILE=$I3_CONFIG_FOLDER/config
+
+SWAY_CONFIG_FOLDER=$HOME/.config/sway
+SWAY_CONFIG_FILE=$SWAY_CONFIG_FOLDER/config
+
 echo "Seting up config files"
 
+echo "Create sway config"
+cat $PROJECT_DIR/desktop.config > $PROJECT_DIR/i3.config
+cat $PROJECT_DIR/i3.utils.config >> $PROJECT_DIR/i3.config
+
+echo "i3 config"
+if [ -e $I3_CONFIG_FILE ] 
+then
+  cp $I3_CONFIG_FILE $I3_CONFIG_FILE.save
+  rm -f $I3_CONFIG_FILE
+fi
+mkdir -p $I3_CONFIG_FOLDER
+ln -s $PROJECT_DIR/i3.config $I3_CONFIG_FILE
+
+echo "Create sway config"
+rm -f $PROJECT_DIR/sway.config
+cat $PROJECT_DIR/desktop.config > $PROJECT_DIR/sway.config
+cat $PROJECT_DIR/sway.background.config >> $PROJECT_DIR/sway.config
+cat $PROJECT_DIR/sway.display.config >> $PROJECT_DIR/sway.config
+cat $PROJECT_DIR/sway.input.config >> $PROJECT_DIR/sway.config
+cat $PROJECT_DIR/sway.utils.config >> $PROJECT_DIR/sway.config
+
 echo "Sway config"
-rm -f $HOME/.config/sway/config
-mkdir -p $HOME/.config/sway
-ln -s $PROJECT_DIR/sway.config $HOME/.config/sway/config
+if [ -e $SWAY_CONFIG_FILE ]
+then
+  cp $SWAY_CONFIG_FILE $SWAY_CONFIG_FILE.save
+  rm -f $SWAY_CONFIG_FILE
+fi
+mkdir -p $SWAY_CONFIG_FOLDER
+ln -s $PROJECT_DIR/sway.config $SWAY_CONFIG_FILE
 
 echo "Swayblocks config"
 rm -f $HOME/.config/swayblocks/config.exs
@@ -18,11 +55,6 @@ ln -s $PROJECT_DIR/swayblocksScripts $HOME/.config/swayblocks/scripts
 echo "Desktop background"
 rm -f $HOME/.config/sway/background.jpg
 ln -s $PROJECT_DIR/background.jpg $HOME/.config/sway/background.jpg
-
-# link i3blocks config file
-echo "i3Blocks config"
-rm -f $HOME/.i3blocks.conf
-ln -s $PROJECT_DIR/i3blocks.conf $HOME/.i3blocks.conf
 
 # Create path to config file and remove if present
 echo "termite config"
